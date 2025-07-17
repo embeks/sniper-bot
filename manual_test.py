8d62e88be9204e7277f4ff854a60f8708290c94b
embeks <emrebek888@gmail.com>
2025-07-17 10:42:44 +1000

    Delete manual_test.py

--- manual_test.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from mempool_listener import manual_trigger
-
-if __name__ == "__main__":
-    token_mint = "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"  # Replace with actual token address
-    asyncio.run(manual_trigger(token_mint))
