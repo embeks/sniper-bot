95680a0c04ddd9ffb072a5b113eb9b21ce85d024
embeks <emrebek888@gmail.com>
2025-07-18 10:39:55 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -139,3 +139,4 @@ async def buy_on_raydium(rpc_client, kp, token, amount):
 # 🧠 Alpha Feed Scanner Stub
 async def scan_alpha_feeds():
     return ["token_mint_example_1", "token_mint_example_2"]
+
