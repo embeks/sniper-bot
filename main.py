7546ee522130bc72a4a7753959b7524eed267863
embeks <emrebek888@gmail.com>
2025-07-21 14:26:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
 from utils import snipe_token
 
-# Direct call bypassing env logic
-async def manual_test():
-    await snipe_token("7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr")
+def manual_test():
+    snipe_token("7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr")
 
-asyncio.run(manual_test())
+if __name__ == "__main__":
+    manual_test()
