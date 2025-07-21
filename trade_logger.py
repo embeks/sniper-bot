7abe298f81249f74e0d03167b93354ce17854e1d
embeks <emrebek888@gmail.com>
2025-07-21 15:14:39 +1000

    Delete trade_logger.py

--- trade_logger.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import csv
-
-def log_trade_to_csv(token, action, amount_in, amount_out):
-    with open("trade_log.csv", "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([datetime.utcnow().isoformat(), token, action, amount_in, amount_out])
