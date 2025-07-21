7c22fa9edad594ffc4c530a1e2dd262705a47a4a
embeks <emrebek888@gmail.com>
2025-07-21 14:33:18 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ async def manual_test():
     await snipe_token("7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr")
 
 if __name__ == "__main__":
-    asyncio.run(manual_test()))
+    asyncio.run(manual_test())
