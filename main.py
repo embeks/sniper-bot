6af6e2514dd9a40c1292db6e2562b972ea613f28
embeks <emrebek888@gmail.com>
2025-07-21 14:31:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
 from utils import snipe_token
 
-def manual_test():
-    snipe_token("7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr")
+async def manual_test():
+    await snipe_token("7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr")
 
 if __name__ == "__main__":
-    manual_test()
+    asyncio.run(manual_test()))
