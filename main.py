e3ba2a586b8a77f43bda50c82ac0a9cbe689f853
embeks <emrebek888@gmail.com>
2025-07-21 16:21:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import asyncio
 from sniper_logic import start_sniper
+from utils import start_command_bot  # make sure this is defined
+
+async def main():
+    await asyncio.gather(
+        start_sniper(),
+        start_command_bot()
+    )
 
 if __name__ == "__main__":
-    asyncio.run(start_sniper())
+    asyncio.run(main())
