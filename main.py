c6f9a6c25da918955455e53ccec55810aaa3fa59
embeks <emrebek888@gmail.com>
2025-07-22 10:09:14 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# =========================
-# main.py — Final
-# =========================
+# =============================
+# main.py — Start Sniper Bot
+# =============================
 
 import asyncio
 from sniper_logic import start_sniper
-from utils import start_command_bot
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_sniper())
-    loop.run_in_executor(None, start_command_bot)
-    loop.run_forever()
+    asyncio.run(start_sniper())
