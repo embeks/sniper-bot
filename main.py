a9be1552b09752bbe4eae9e3c1b39c20985478ce
embeks <emrebek888@gmail.com>
2025-07-25 17:07:05 +1000

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# =========================
-# main.py — Launch Bot
-# =========================
-
-import asyncio
-from sniper_logic import start_sniper
-
-if __name__ == "__main__":
-    asyncio.run(start_sniper())
