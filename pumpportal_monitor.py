45afe612aca785a0eb0baf0fd3eb2dfc9a913763
embeks <emrebek888@gmail.com>
2025-10-07 16:20:10 +1100

    Update pumpportal_monitor.py

--- pumpportal_monitor.py
+++ pumpportal_monitor.py
@@ -49,7 +49,7 @@ class PumpPortalMonitor:
             'max_curve_sol': 60.0,
             'min_v_tokens': 500_000_000,
             'min_name_length': 3,
-            'min_holders': 6,  # CRITICAL: Minimum 5 holders required
+            'min_holders': 12,  # CRITICAL: Minimum 5 holders required
             'check_concentration': False,  # DISABLED FOR TESTING - Will re-enable after data collection
             'max_top10_concentration': 85,  # Not enforced when check_concentration=False
             'max_velocity_sol_per_sec': 1.5,
